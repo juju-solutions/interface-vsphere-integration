1252d4c67b0ce1dd646475a5011ff90d9f1f1ad4
Kevin W Monroe <kevin.monroe@canonical.com>
2018-08-17 15:48:57 +0000

    initial commit based on interface-openstack-integration

---
+++ provides.py
@@ -0,0 +1,110 @@
+"""
+This is the provides side of the interface layer, for use only by the
+vSphere integration charm itself.
+
+The flags that are set by the provides side of this interface are:
+
+* **`endpoint.{endpoint_name}.requested`** This flag is set when there is
+  a new or updated request by a remote unit for vSphere integration
+  features.  The vSphere integration charm should then iterate over each
+  request, perform whatever actions are necessary to satisfy those requests,
+  and then mark them as complete.
+"""
+
+from operator import attrgetter
+
+from charms.reactive import Endpoint
+from charms.reactive import when
+from charms.reactive import toggle_flag, clear_flag
+
+
+class VsphereIntegrationProvides(Endpoint):
+    """
+    Example usage:
+
+    ```python
+    from charms.reactive import when, endpoint_from_flag
+    from charms import layer
+
+    @when('endpoint.vsphere.requests-pending')
+    def handle_requests():
+        vsphere = endpoint_from_flag('endpoint.vsphere.requests-pending')
+        for request in vsphere.requests:
+            request.set_credentials(layer.vsphere.get_user_credentials())
+        vsphere.mark_completed()
+    ```
+    """
+
+    @when('endpoint.{endpoint_name}.changed')
+    def check_requests(self):
+        toggle_flag(self.expand_name('requests-pending'),
+                    len(self.requests) > 0)
+        clear_flag(self.expand_name('changed'))
+
+    @property
+    def requests(self):
+        """
+        A list of the new or updated #IntegrationRequests that
+        have been made.
+        """
+        if not hasattr(self, '_requests'):
+            all_requests = [IntegrationRequest(unit)
+                            for unit in self.all_joined_units]
+            is_changed = attrgetter('is_changed')
+            self._requests = list(filter(is_changed, all_requests))
+        return self._requests
+
+    def mark_completed(self):
+        """
+        Mark all requests as completed and remove the `requests-pending` flag.
+        """
+        clear_flag(self.expand_name('requests-pending'))
+        self._requests = []
+
+
+class IntegrationRequest:
+    """
+    A request for integration from a single remote unit.
+    """
+    def __init__(self, unit):
+        self._unit = unit
+
+    @property
+    def _to_publish(self):
+        return self._unit.relation.to_publish
+
+    @property
+    def is_changed(self):
+        """
+        Whether this request has changed since the last time it was
+        marked completed (if ever).
+        """
+        return not self.has_credentials
+
+    @property
+    def unit_name(self):
+        return self._unit.unit_name
+
+    def set_credentials(self,
+                        auth_url,
+                        username,
+                        password,
+                        datacenter,
+                        datastore):
+        """
+        Set the credentials for this request.
+        """
+        self._unit.relation.to_publish.update({
+            'auth_url': auth_url,
+            'username': username,
+            'password': password,
+            'datacenter': datacenter,
+            'datastore': datastore,
+        })
+
+    @property
+    def has_credentials(self):
+        """
+        Whether or not credentials have been set via `set_credentials`.
+        """
+        return 'credentials' in self._unit.relation.to_publish
---
+++ requires.py
@@ -0,0 +1,113 @@
+"""
+This is the requires side of the interface layer, for use in charms that wish
+to request integration with vSphere native features.  The integration will be
+provided by the vSphere integration charm, which allows the requiring charm
+to not require cloud credentials itself and not have a lot of vSphere
+specific API code.
+
+The flags that are set by the requires side of this interface are:
+
+* **`endpoint.{endpoint_name}.joined`** This flag is set when the relation
+  has been joined, and the charm should then use the methods documented below
+  to request specific vSphere features.  This flag is automatically removed
+  if the relation is broken.  It should not be removed by the charm.
+
+* **`endpoint.{endpoint_name}.ready`** This flag is set once the requested
+  features have been enabled for the vSphere instance on which the charm is
+  running.  This flag is automatically removed if new integration features are
+  requested.  It should not be removed by the charm.
+"""
+
+
+from charms.reactive import Endpoint
+from charms.reactive import when, when_not
+from charms.reactive import clear_flag, toggle_flag
+
+
+class VsphereIntegrationRequires(Endpoint):
+    """
+    Interface to request integration access.
+
+    Note that due to resource limits and permissions granularity, policies are
+    limited to being applied at the charm level.  That means that, if any
+    permissions are requested (i.e., any of the enable methods are called),
+    what is granted will be the sum of those ever requested by any instance of
+    the charm on this cloud.
+
+    Labels, on the other hand, will be instance specific.
+
+    Example usage:
+
+    ```python
+    from charms.reactive import when, endpoint_from_flag
+
+    @when('endpoint.vsphere.ready')
+    def vsphere_integration_ready():
+        vsphere = endpoint_from_flag('endpoint.vsphere.joined')
+        update_config_enable_vsphere(vsphere.auth_url,
+                                     vsphere.username,
+                                     vsphere.password,
+                                     vsphere.datacenter,
+                                     vsphere.datastore)
+    ```
+    """
+
+    @property
+    def _received(self):
+        """
+        Helper to streamline access to received data since we expect to only
+        ever be connected to a single vSphere integration application with a
+        single unit.
+        """
+        return self.relations[0].joined_units.received
+
+    @property
+    def _to_publish(self):
+        """
+        Helper to streamline access to received data since we expect to only
+        ever be connected to a single vSphere integration application with a
+        single unit.
+        """
+        return self.relations[0].to_publish
+
+    @when('endpoint.{endpoint_name}.changed')
+    def check_ready(self):
+        toggle_flag(self.expand_name('ready'), self.is_ready)
+        clear_flag(self.expand_name('changed'))
+
+    @when_not('endpoint.{endpoint_name}.joined')
+    def remove_ready(self):
+        clear_flag(self.expand_name('ready'))
+
+    @property
+    def is_ready(self):
+        """
+        Whether or not the request for this instance has been completed.
+        """
+        return all(field is not None for field in [
+            self.auth_url,
+            self.username,
+            self.password,
+            self.datacenter,
+            self.datastore,
+        ])
+
+    @property
+    def auth_url(self):
+        return self._received['auth_url']
+
+    @property
+    def username(self):
+        return self._received['username']
+
+    @property
+    def password(self):
+        return self._received['password']
+
+    @property
+    def datacenter(self):
+        return self._received['datacenter']
+
+    @property
+    def datastore(self):
+        return self._received['datastore']
